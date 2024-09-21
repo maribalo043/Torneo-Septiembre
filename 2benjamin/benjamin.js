20543dd3cc0ff3e32b4779e75291208f7f511049
maribalo <133330755+maribalo043@users.noreply.github.com>
2024-09-21 11:29:24 +0200

    Actualizar benjamin.js

--- 2benjamin/benjamin.js
+++ 2benjamin/benjamin.js
@@ -145,9 +145,9 @@ var tenis = new Equipo("Tenis");
 
 // Crear instancias de partidos
 var partido1 = new Partido(patinalon, corre,'4-0',"Viernes", "17:45 - 18:30", "Villa",true);
-var partido2 = new Partido(tenis, pilar, '0-0',"Sabado", "10:45 - 11:30", "Villa",false);
-var TercerCuarto = new PartidoFinal('Indeterminado', 'Indeterminado', '0-0',"Sabado", "14:30 - 15:15", "Villa",false);
-var Final = new PartidoFinal('Indeterminado', 'Indeterminado', '0-0',"Sabado", "17:30 - 18:15", "Villa",false);
+var partido2 = new Partido(tenis, pilar, '4-8',"Sabado", "10:45 - 11:30", "Villa",true);
+var TercerCuarto = new PartidoFinal(tenis, corre, '0-0',"Sabado", "14:30 - 15:15", "Villa",false);
+var Final = new PartidoFinal(pilar, patinalon, '0-0',"Sabado", "17:30 - 18:15", "Villa",false);
 
 function ordenarClasificacion(datosClasificacion) {
     datosClasificacion.sort(function(a, b) {
