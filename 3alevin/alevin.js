5e74b1de366df564321da026d633c410ef91fd6d
maribalo <133330755+maribalo043@users.noreply.github.com>
2024-09-21 16:03:51 +0200

    Actualizar alevin.js

--- 3alevin/alevin.js
+++ 3alevin/alevin.js
@@ -146,7 +146,7 @@ var tenis = new Equipo("Tenis");
 // Crear instancias de partidos
 var partido1 = new Partido(patinalon, pilar, '8-0',"Viernes", "18:30 - 19:15", "Villa",true);
 var partido2 = new Partido(tenis, roller, '1-3',"Sabado", "11:30 - 12:15", "Villa",true);
-var TercerCuarto = new PartidoFinal(pilar,tenis, '0-0',"Sabado", "15:15 - 16:00", "Villa",false);
+var TercerCuarto = new PartidoFinal(pilar,tenis, '0-5',"Sabado", "15:15 - 16:00", "Villa",true);
 var Final = new PartidoFinal(patinalon, roller, '0-0',"Sabado", "18:15 - 19:00", "Villa",false);
 
 function ordenarClasificacion(datosClasificacion) {
